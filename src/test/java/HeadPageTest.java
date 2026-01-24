3cc1de163f340203682c03fd4ee7cb5194a9da44
Vlad Sentyakov <vsentyakov@yandex.ru>
2026-01-24 17:50:52 +0300

    commit_new_test

--- src/test/java/HeadPageTest.java
+++ src/test/java/HeadPageTest.java
@@ -1,26 +1,31 @@
 import org.example.HeadPage;
 import org.example.PageAuth;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class HeadPageTest extends BaseTest{
 
+public class HeadPageTest extends BaseTest {
+
+    private HeadPage headPage;
 
     private static final String VALID_LOGIN = "testovik";
     private static final String VALID_PASSWORD = "123456";
-    private static final String NAME_BLOCK ="test";
+    private static final String NAME_BLOCK = "test";
 
-    //Создание блока
+    @Before
+    public void initPage() {
+        headPage = new HeadPage(driver);
+    }
+
+    // Создание блока
     @Test
-    public void CreateBlock(){
-        HeadPage headPage = new HeadPage(driver);
+    public void createBlock() {
         headPage
-                .login(VALID_LOGIN,VALID_PASSWORD)
+                .login(VALID_LOGIN, VALID_PASSWORD)
                 .sendNameBlock(NAME_BLOCK)
                 .clickButtonAddBlock();
 
         Assert.assertTrue("Цель не создана", headPage.goalsBlockIsVisible());
-
-
     }
 }
